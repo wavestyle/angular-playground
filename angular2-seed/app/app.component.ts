05107a0b72667528d2974a180687fdf8d098e106
Adrian Heydecker <adrian.heydecker@netcetera.com>
2017-03-18 15:51:52 +0100

    running version with course service and dependency injection

--- angular2-seed/app/app.component.ts
+++ angular2-seed/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component, View} from 'angular2/core';
-import {CoursesComponent} from './courses.component'
+import {Component} from 'angular2/core';
+import {CoursesComponent} from './courses.component';
 
 @Component({
     selector: 'my-app',
--- angular2-seed/app/courses.component.ts
+++ angular2-seed/app/courses.component.ts
@@ -1,10 +1,25 @@
-import {Component} from 'angular2/core'
+import { Component } from 'angular2/core';
+import { CourseService } from "./course.service";
 
 @Component({
     selector: 'courses',
-    template: '<h2>courses</h2>'
+    template: `
+    <h2>courses</h2>
+    {{title}}
+    <ul>
+     <li *ngFor="#course of courses">
+         {{course}}
+    </li>
+    </ul>
+    `,
+    providers: [CourseService]
 })
 
 export class CoursesComponent {
     title = "The title of courses page";
+    //courses =  ["Course 1", "Course 2", "Course 3"];
+    courses;
+    constructor(courseService: CourseService){
+        this.courses = courseService.getCourses();
+    }
 }
